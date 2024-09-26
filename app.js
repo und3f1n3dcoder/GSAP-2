61a8b37fb3b65cc0753ec9d9a517058b12de9a61
Amaan <3133@online.dooninternational.net>
2024-09-26 06:32:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".square", {
+//   x: 500,
+//   duration: 3,
+//   scrollTrigger: {
+//     trigger: ".square",
+//     start: "top 50%", //pixels
+//     markers: true,
+//     // toggleClass: "red",
+//   },
+// });
+// gsap.to(".square", {
+//   duration: 2,
+//   x: 300,
+//   scale: 1.5,
+//   rotation: 180,
+//   backgroundColor: "#3498db",
+//   ease: "elastic",
+//   delay: 1,
+//   repeat: -1,
+//   yoyo: true,
+//   onComplete: () => console.log("Animation complete!"),
+//   onUpdate: () => console.log("Updating..."),
+//   onStart: () => console.log("Animation started!"),
+// });
+
+gsap.from(".demo", {
+  opacity: 0,
+  duration: 1.5,
+  y: -100,
+  stagger: 0.5,
+  scale: 1.2,
+  ease: "circ",
+  skewX: 20,
+  skewY: 20,
+  rotateX: 90,
+});
+
+gsap.from(".demo1", {
+  scrollTrigger: {
+    trigger: ".demo1",
+    start: "top 50%",
+    markers: true,
+  },
+  opacity: 0,
+  duration: 1.5,
+  y: 100,
+  stagger: 0.5,
+  scale: 1.2,
+  ease: "circ",
+  skewX: 20,
+  skewY: 20,
+  rotateX: 90,
+});
